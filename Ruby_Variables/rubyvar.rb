4900d83171cbf40decb9774f5f707b9993dbd240
Spencer Meredith <Smeredith1119@gmail.com>
2019-07-12 14:21:22 -0700

    Ruby Lesson 1 Working With puts methods

---
+++ Ruby_Variables/rubyvar.rb
@@ -0,0 +1,19 @@
+# This should print out Hello World
+# puts is a METHOD  in Ruby Langauge
+# when using the method puts it is used to print out on the screen.
+# In the parentheses "" is wrap around the world Hello World to idenfiy/create it as a string 
+# Without the "" wrapped around the word Hello World it will create a syntax error
+# Comment out to see the differnces.
+# For example to see idenfiy the differces
+
+
+
+# To make this line of code to work uncomment line 13
+# 1 This will work
+puts ("Hello World")  
+
+
+
+# To show the incorrect way uncomment line 19
+ # 2  this will not work
+# puts (Hello World) 
